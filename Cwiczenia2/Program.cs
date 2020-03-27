21c5cbccec76c042175b782b4fdb797185c2432f
S17186 <61650510+S17186@users.noreply.github.com>
2020-03-27 22:20:42 +0100

    Changed Console.log
    Some unimportant changes for practice.

--- Cwiczenia2/Program.cs
+++ Cwiczenia2/Program.cs
@@ -6,7 +6,8 @@ namespace Cwiczenia2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World !");
+            Console.WriteLine("Bye World !");
         }
     }
 }
